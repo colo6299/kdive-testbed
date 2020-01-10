d456cfc2fc35b75e699806f2c71b910a1e50de99
= <=>
2020-01-10 12:52:32 -0800

    Updated arm preditor

--- arduino/FDVR_/FDVR_.ino
+++ arduino/FDVR_/FDVR_.ino
@@ -5,14 +5,15 @@ int bicepPin = A1;
 int tricepPin = A0;  
 
 int lastTime = 0;
-float armAngle = 0.0f;
-float armVelocity = 0.0f;
+float armAngle = 0.0;
+float armVelocity = 0.0;
+const float amp = 1.0;
 const float g = -9.81;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(19200);
 }
 
 // the loop routine runs over and over again forever:
@@ -21,16 +22,15 @@ void loop() {
   int bicepValue = analogRead(bicepPin);
   int tricepValue = analogRead(tricepPin);
 
-  // this is just debug code to fix the auto-range on the
+  // this is just debug code to change the auto-range on the
   //     arduino ide serial monitor...
-
-  /*
   if ((millis() % 500) < 15)
   {
     Serial.println(0);
     Serial.println(1000);
+    armVelocity += 1.0;
   }
-  */
+  
 
   //so you know how much of a second passed for force calculations
   float deltaTime = (millis() - lastTime) / 1000;
@@ -39,24 +39,32 @@ void loop() {
     deltaTime = 0;
   }
   
+  //this roughly simulates the effect of the real muscles on the virtual arm
+  armVelocity += bicepValue - tricepValue * amp * deltaTime;
+  
+  
   //the cosine calculates the force gravity exerts on a pivot
   //    at a particular angle. 
-  armVelocity = cos(armAngle) * g * deltaTime;
-  armAngle -= armVelocity * deltaTime;
+  armVelocity += cos(armAngle) * g * deltaTime;
+  armAngle += armVelocity * deltaTime;
 
 
-  //stops for the arm, most of our elbows can't do 360's
+  //stops for the arm, most of our elbows can't do 360's 
+  /*
   if (armAngle <= 0) {
     armAngle = 0;
-    armVelocity = 0;
+    if (armVelocity < 0) {
+      armVelocity = 0;
+    }
   }
   if (armAngle >= 1){
     armAngle = 1;
     armVelocity = 0;
-  }
-
-  Serial.println(int(armAngle) * 1000);
+  }*/
+  
+  //scale it up to look nice, and print!
+  Serial.println(armAngle * 1000);
 
  
-  delay(1);
+  delay(2);
 }
